43e0b0eb3790f8861f2ac504a9bc7fc5d106ae9a
Shaun Savage <swansavage@gmail.com>
2018-03-30 10:35:16 -0400

    Set up basic server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+
+
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
+})
+
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/SongSaver');
+mongoose.connection.once('open', ()=>{
+    console.log(*********************);
+    console.log('Connected to Mongoose');
+    console.log(*********************);
+});
+
+app.listen(port, ()=>{
+    console.log(=====================);
+    console.log('Listening on Port: ' + port);
+})
