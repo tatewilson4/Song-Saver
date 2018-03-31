f7208c31267a008c83642e694d5523402d1d17d7
Tate Wilson <tatewilson4@gmail.com>
2018-03-30 18:29:21 -0600

    added update and delete to index and app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,32 @@
 const app = angular.module('songs', []);
 
 app.controller('mainController' , ['$http', function($http){
+    this.indexOfEditFormToShow = null;
     
+    this.editSongs = ( song ) => {
+    $http({
+        method: 'PUT',
+        url: '/songs/' + song._id,
+        data: {
+            song: this.song,
+            albumn: this.albumn,
+            cover: this.cover
+        }
+    }).then( response => {
+        this.getSongs();
+    }, error => {
+        console.log( error );
+    });
+}
+
+this.deleteSong = ( song ) => {
+    $http({
+        method: 'DELETE',
+        url: '/songs/' + song._id
+    }).then( response => {
+        controller.getSongs();
+    }, error => {
+        console.log( error );
+    });
+}
 }])
