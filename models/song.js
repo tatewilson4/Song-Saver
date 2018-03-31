151de8096d2c45ef5ad287dd1534da230efcfcbb
Shaun Savage <swansavage@gmail.com>
2018-03-30 20:12:26 -0400

    added GET and POST routes // changed model 'name' to 'title'

--- models/song.js
+++ models/song.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const songSchema = new mongoose.Schema({
-    song: String,
+    title: String,
     albumn: String,
     cover: {type: String, default: 'https://cdn.tutsplus.com/net/uploads/legacy/958_placeholders/placehold.gif'}
 });
